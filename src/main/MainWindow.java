e4ade7110697e2d3d92f848c67586ccdb6289979
John Doe <doej1367@gmail.com>
2021-10-06 16:48:01 +0200

    added support for .log files (only .log.gz files before)

--- src/main/MainWindow.java
+++ src/main/MainWindow.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.attribute.BasicFileAttributes;
@@ -162,8 +163,10 @@ public class MainWindow extends JFrame {
 			for (File logFile : minecraftLogFolder.listFiles()) {
 				try {
 					long creationTime = getCreationTime(logFile);
-					br = new BufferedReader(
-							new InputStreamReader(new GZIPInputStream(new FileInputStream(logFile)), "UTF-8"));
+					InputStream inputStream = new FileInputStream(logFile);
+					if (logFile.getName().endsWith(".gz"))
+						inputStream = new GZIPInputStream(inputStream);
+					br = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"));
 					List<LogLine> tmp = br.lines().filter(a -> {
 						if (a.matches("\\[[0-9:]{8}\\] \\[Client thread/INFO\\]: Setting user: .*")) {
 							String name = a.replaceAll("\\[[0-9:]{8}\\] \\[Client thread/INFO\\]: Setting user: ", "");
